292e823999445240628d9107c9c7d990c40abc3c
t.h <hamanoty1991@gmail.com>
2022-11-01 23:39:19 +0900

    11/1 work

--- ABC/275/c.py
+++ ABC/275/c.py
@@ -1,36 +1,23 @@
-s = [input() for _ in range(9)]
-cnt =0
-d = {}
+from itertools import combinations
+
+v = []
+cnt = 0
 
 for i in range(9):
+    s = input()
     for j in range(9):
-        for k in range(9):
-            for l in range(9):
-                if i==k and j==l:
-                    continue
-                
-                v1 = s[i][j]
-                v2 = s[k][l]
-                #v3 = s[i-j+l][j+i-k]
-                #v4 = s[k-j+l][l+i-k]
-                dx, dy = k-i, l-i
-                if 0<=k-dy<9 and 0<=l+dx<9 and 0<=i-dy<9 and 0<=l+dx<9:
-                    v3 = s[k-dy][l+dx]
-                    v4 = s[i-dy][l+dx]
-                    if v1=='#' and v2=='#' and v3=='#' and v4=='#':
-                        w = str(i)+','+str(j)+','+str(i-dy)+','+str(l+dx)
-                        if d.get(w, 'hoge') == 'hoge':
-                            cnt += 1
-                            d[w] = 1
-                            continue
-                elif 0<=k+dy<9 and 0<=l-dx<9 and 0<=i+dy<9 and 0<=l-dx<9:
-                    v3 = s[k+dy][l-dx]
-                    v4 = s[i+dy][l-dx]
-                    if v1=='#' and v2=='#' and v3=='#' and v4=='#':
-                        w = str(i)+','+str(j)+','+str(i+dy)+','+str(l-dx)
-                        if d.get(w, 'hoge') == 'hoge':
-                            cnt += 1
-                            d[w] = 1
-                            continue
+        if s[j] == '#':
+            v.append([i,j])
+
+for i in combinations(v,4):
+    distance = set()
+    for j in combinations(i,2):
+        d = (j[0][0]-j[1][0])**2+(j[0][1]-j[1][1])**2
+        distance.add(d)
+    
+    if len(distance) == 2:
+        cnt += 1
+
 print(cnt)
-print(d)
+    
+
